ed4f642e71c1326eb16d44bec6aa8643eb585943
musicoariel4 <67813547+musicoariel4@users.noreply.github.com>
2020-07-18 18:46:37 -0500

    primer commit

---
+++ app/src/main/java/com/musicoariel4/imagen_corporativa_ariel/MainActivity.java
@@ -0,0 +1,14 @@
+package com.musicoariel4.imagen_corporativa_ariel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
